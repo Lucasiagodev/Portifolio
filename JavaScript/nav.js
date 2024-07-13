cd2e27ebf48746cbc9b91b829b30f3ca7393bb49
Lucasiagodev <lucasiagodev@gmail.com>
2024-07-12 22:51:45 -0300

    ds

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,10 +1,12 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+    let icon = document.querySelector('.icon');
+
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "menu_white_36dp.svg";
+        icon.src = "menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "close_white_36dp.svg";
+        icon.src = "close_white_36dp.svg";
     }
 }
